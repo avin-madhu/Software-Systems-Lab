9fb1d580feb4e9d880ccc898cd07d484718ddcb6
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-12-09 13:15:37 +0530

    Update absolute.c

--- assembler/absolute/absolute.c
+++ assembler/absolute/absolute.c
@@ -1,101 +1,53 @@
 #include <stdio.h>
 #include <string.h>
-char input[10], label[10], ch1, ch2;
-int addr, w = 0, start, ptaddr, l, length = 0, end, count = 0, k, taddr, address, i = 0;
-FILE *fp1, *fp2;
-void check();
-void main()
+#include <stdlib.h>
+int main()
 {
-	fp1 = fopen("input.txt", "r");
-	fp2 = fopen("output.txt", "w");
-	fscanf(fp1, "%s", input);
-	printf("\n\nABSOLUTE LOADER\n");
-	fprintf(fp2, "\n-------------------------------------------------------\n");
-	fprintf(fp2, "MEMORY ADDRESS\t\t\tCONTENTS");
-	fprintf(fp2, "\n-------------------------------------------------------\n");
-	while (strcmp(input, "E") != 0)
+	FILE *fp;
+	int i, j, staddr1;
+	char line[50],name1[50],staddr[200];
+
+	fp = fopen("ab.txt", "r");
+	fscanf(fp, "%s", line);
+
+	for(i=2,j=0;line[i]!='^';i++,j++)
 	{
-		if (strcmp(input, "H") == 0)
+		name1[j] = line[i];
+	}
+	name1[j] = '\0';
+	printf("\nName from Object file: %s\n\n",name1);
+
+    while(!feof(fp))
+	{
+		fscanf(fp, "%s", line);
+		while(line[0]=='T')
 		{
-			fscanf(fp1, "%s %x %x %s", label, &start, &end, input);
-			address = start;
-		}
-		else if (strcmp(input, "T") == 0)
-		{
-			l = length;
-			ptaddr = addr;
-			fscanf(fp1, "%x %x %s", &taddr, &length, input);
-			addr = taddr;
-			if (w == 0)
+			for(i=2,j=0;line[i]!='^';i++,j++)
 			{
-				ptaddr = address;
-				w = 1;
+				staddr[j] = line[i]; 
 			}
-			for (k = 0; k < (taddr - (ptaddr + l)); k++)
+			staddr[j] = '\0';
+			staddr1 = atoi(staddr);
+
+			i=12;
+			while(line[i]!='$')
 			{
-				address = address + 1;
-				fprintf(fp2, "xx");
-				count++;
-				if (count == 4)
+				if(line[i]!='^')
 				{
-					fprintf(fp2, "  ");
+                    printf("00%d\t%c%c\n",staddr1,line[i],line[i+1]);
+					staddr1++;
+					i+=2;
+				}
+				else
+				{
 					i++;
-					if (i == 4)
-					{
-						fprintf(fp2, "\n\n%x\t\t", address);
-						i = 0;
-					}
-					count = 0;
 				}
 			}
-			if (taddr == start)
-				fprintf(fp2, "\n\n%x\t\t", taddr);
-			fprintf(fp2, "%c%c", input[0], input[1]);
-			check();
-			fprintf(fp2, "%c%c", input[2], input[3]);
-			check();
-			fprintf(fp2, "%c%c", input[4], input[5]);
-			check();
-			fscanf(fp1, "%s", input);
+			fscanf(fp,"%s",line);
 		}
-		else
+	}
+	if(line[0]=='E')
 		{
-			fprintf(fp2, "%c%c", input[0], input[1]);
-			check();
-			fprintf(fp2, "%c%c", input[2], input[3]);
-			check();
-			fprintf(fp2, "%c%c", input[4], input[5]);
-			check();
-			fscanf(fp1, "%s", input);
+			fclose(fp);
 		}
-	}
-	fprintf(fp2, "\n-------------------------------------------------------\n");
-	fclose(fp1);
-	fclose(fp2);
-	printf("\n\n The contents of output file:\n");
-	fp2 = fopen("output.txt", "r");
-	ch2 = fgetc(fp2);
-	while (ch2 != EOF)
-	{
-		printf("%c", ch2);
-		ch2 = fgetc(fp2);
-	}
-	fclose(fp2);
-}
-void check()
-{
-	count++;
-	address++;
-	taddr = taddr + 1;
-	if (count == 4)
-	{
-		fprintf(fp2, "  ");
-		i++;
-		if (i == 4)
-		{
-			fprintf(fp2, "\n\n%x\t\t", taddr);
-			i = 0;
-		}
-		count = 0;
-	}
 }
