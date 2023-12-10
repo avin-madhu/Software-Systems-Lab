faef33f64105dd123b3e547e27371280d7540bc8
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-12-10 16:05:41 +0530

    Update relocation.c

--- assembler/reloc/relocation.c
+++ assembler/reloc/relocation.c
@@ -1,54 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 int main()
 {
-	FILE *fp;
-	int i, j, staddr1;
-	char line[50],name1[50],staddr[200];
+    FILE *fp;
+    char line[200], name[100],stradd[10];
+    int i, j, intadd, rel;
 
-	fp = fopen("ab.txt", "r");
-	fscanf(fp, "%s", line);
+    fp = fopen ("obj.txt", "r");
+    fscanf(fp, "%s", line);
 
-	for(i=2,j=0;line[i]!='^';i++,j++)
-	{
-		name1[j] = line[i];
-	}
-	name1[j] = '\0';
-	printf("\nName from Object file: %s\n\n",name1);
+    //displaying the name of the object file
 
+    for( i=2, j=0;line[i]!='^';i++, j++)
+    {
+        name[j] = line[i];
+    }
+    name[j] = '\0';
+
+    printf("The name of the object file is: %s\n\n", name);
+
+    printf("Enter the relocation address: ");
+    scanf("%d", &rel);
+    
     while(!feof(fp))
-	{
-		fscanf(fp, "%s", line);
-		while(line[0]=='T')
-		{
-			for(i=2,j=0;line[i]!='^';i++,j++)
-			{
-				staddr[j] = line[i]; 
-			}
-			staddr[j] = '\0';
-			staddr1 = atoi(staddr);
+    {
+        fscanf(fp, "%s", line);
 
-			i=12;
-			while(line[i]!='$')
-			{
-				if(line[i]!='^')
-				{
-                    printf("00%d\t%c%c\n",staddr1,line[i],line[i+1]);
-					staddr1++;
-					i+=2;
-				}
-				else
-				{
-					i++;
-				}
-			}
+        while(line[0]=='T')
+        {
+            for(i=2,j=0;line[i]!='^';i++, j++)
+            {
+                stradd[j] = line[i];
+            }
+            stradd[j]='\0';
+            intadd = atoi(stradd);
+            intadd+=rel;
+            
+        
+             i=12;
 
-			fscanf(fp,"%s",line);
-		}
-	}
-	if(line[0]=='E')
-		{
-			fclose(fp);
-	        }
+        while(line[i]!='$')
+        {
+           if(line[i]!='^')
+           {
+            printf("%05d\t%c%c\n", intadd, line[i], line[i+1]);
+            i+=2;
+            intadd+=rel;
+           }
+           else
+           {
+            i++;
+           }
+        }
+
+         fscanf(fp, "%s", line);
+        } 
+    }
+    if(line[0]=='E')
+    {
+        fclose(fp);
+        printf("\nObject Code loaded successfully!");
+    }
 }
