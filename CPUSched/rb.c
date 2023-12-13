cf42b86230102f55043affe58ebe146fbaf68810
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-12-13 19:30:28 +0530

    Update rb.c

--- CPUSched/rb.c
+++ CPUSched/rb.c
@@ -1,55 +1,93 @@
 #include <stdio.h>
+int q[100], f=-1, r=-1;
+void insert(int n)
+{
+   if(f==-1) // queue being empty 
+   {
+     f=0;
+   }
+   r=r+1;
+   q[r] = n;
+}
+int delete()
+{
+    int n;
+    n = q[f];
+    f++;
+    return n;
+}
 int main()
 {
-    int n,remain, at[10],bt[10],q,time;
-    int rt[10],flag=0,sumWait=0,sumturnAroundTime=0;
-    printf("Enter the number of process: ");
-    scanf("%d",&n);
-    remain=n;
-    printf("Enter the Burst time and arrival time for each process: \n\n");
+    int n,p,t=0;
+    float ttat=0,twt=0;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    int a[n][5], bt[10], exist[10] = {0}, tq=0;
+    //reading arrival time and burst time
+    printf("Enter the arrival time and burst time of each process: \n");
+
+    for(int i=0; i<n; i++)
+    {
+        printf("Process ID: ");
+        scanf("%d", &a[i][0]);
+        printf("Arrival time: ");
+        scanf("%d", &a[i][1]);
+        printf("Burst time: ");
+        scanf("%d", &a[i][2]);
+        printf("\n");
+
+        bt[i] = a[i][2];
+    }
+
+    printf("Enter the time quantum: ");
+    scanf("%d", &tq);
+
+    insert(0);
+    exist[0] = 1;
+
+    while(f<=r)
+    {
+        p = delete();
+        if(a[p][2]>=tq)
+        {
+            a[p][2]-=tq;
+            t+=tq; // to keep track of time elapsed
+        }
+        else
+        {
+            t+=a[p][2];
+            a[p][2]=0;
+        }
+
+        for(int i=0;i<n;i++) // enter the ready queue if the arrival time < time elapsed
+        {
+            if(exist[i]==0 && a[i][1]<=t)
+            {
+                insert(i);
+                exist[i]=1;
+            }
+        }
+
+        if(a[p][2]==0)
+        {
+            a[p][4] = t - a[p][1];
+            a[p][3] = a[p][4]-bt[p];
+            ttat = ttat + a[p][4];
+            twt = twt + a[p][3];
+        }
+        else
+        {
+            insert(p);
+        }
+    }
+
+    printf("\tPRO\tAT\tBT\tTAT\tWT\n");
     for(int i=0;i<n;i++)
     {
-        printf("p%d: ",i+1);
-        scanf("%d %d", &at[i],&bt[i]);
-        rt[i] = bt[i];
+        printf("%d\t%d\t%d\t%d\t%d\n", a[i][0], a[i][1], bt[i], a[i][4], a[i][3]);
     }
-    printf("Enter the Quantum Time: ");
-    scanf("%d",&q);
-    for(int i=0,time=0;remain!=0;)
-    {
-         if(rt[i]<=q && rt[i]>0)// still quantum time left
-         {
-            time+=rt[i];
-            rt[i]=0;
-            flag=1;
-         }
-         else if(rt[i]>0)
-         {
-            rt[i]-=q;
-            time+=q;
-         }
-         if(rt[i]==0&&flag==1)// completed execution of a process
-         {
-            remain--;
-            sumWait+=time-at[i]-bt[i];
-            sumturnAroundTime+=time-at[i];
-            flag=0;
-         }
-
-         if(i==n-1)
-         {
-            i=0;
-         }
-         else if(at[i+1]<=time)
-         {
-            i++;
-         }
-         else
-         {
-            i=0;
-         }
-    }
-    printf("\nAverage Waiting Time: %f\n",(sumWait*1.0)/n);
-    printf("\nAverage turn Around time: %f",(sumturnAroundTime*1.0)/n);
-
+    printf("Average turnaround time: %f\n", ttat/n);
+    printf("Average waitinng time: %f\n", twt/n);
+    return 0;
 }
+
